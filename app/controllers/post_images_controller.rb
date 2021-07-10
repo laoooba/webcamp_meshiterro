2add1d1f0a12f4214581dfe0a8a3a8a0dab80b90
laoooba <yama10taku31@gmail.com>
2021-07-10 07:49:31 +0000

    [start]4-3

---
+++ app/controllers/post_images_controller.rb
@@ -0,0 +1,23 @@
+class PostImagesController < ApplicationController
+  def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_iamge = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id
+    @post_iamge.save
+    redirect_to post_iamge_path
+  end
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def destroy
+  end 
+  
+  
+end
