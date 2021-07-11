37b5ed019b7c369ebb42d006446ae90e50029b5f
laoooba <yama10taku31@gmail.com>
2021-07-11 07:07:05 +0000

    [finish]4-5

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,5 +8,7 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :post_comments,only: [:create, :destroy]
   end
+  resources :users, only: [:show, :edit, :update]
+  
 
 end
