1e1723009bef3372b89bf6ae8d38996360cd44e8
laoooba <yama10taku31@gmail.com>
2021-07-11 02:04:29 +0000

    [add]4-4

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments,only: [:create, :destroy]
+  end
 
 end
